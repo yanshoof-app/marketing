8abbdecc93a7cababacbf63a91eb310d84559267
Itay Oshri <itay.oshri@gmail.com>
2022-04-30 15:22:27 +0300

    FEATURE: Copy components

---
+++ components/forms/Buttons.tsx
@@ -0,0 +1,34 @@
+import { Wrapper } from "../types";
+
+const variants = {
+  primary: "bg-sky-500 text-white sm:hover:bg-sky-700",
+  secondary:
+    "bg-transparent text-sky-500 ring-1 ring-sky-500 hover:text-sky-700 hover:ring-sky-700",
+};
+
+export type ButtonVariant = keyof typeof variants;
+
+export interface ButtonProps {
+  variant?: ButtonVariant;
+  onClick?(): unknown;
+  className?: string;
+  disabled?: boolean;
+}
+
+export default function Button({
+  variant = "primary",
+  onClick = () => {},
+  children,
+  className = "",
+  disabled = false,
+}: ButtonProps & Wrapper) {
+  return (
+    <button
+      className={`rounded-lg px-4 py-2 font-semibold disabled:opacity-60 ${className}`}
+      onClick={() => onClick()}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
---
+++ components/forms/Input.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+type Variant = "default" | "lesson";
+
+export interface InputProps {
+  onChange?(string): unknown;
+  value: string;
+  hint: string;
+  className?: string;
+}
+
+export default function Input({
+  value,
+  onChange = () => {},
+  hint,
+  className = "",
+}: InputProps) {
+  const [input, setInput] = useState(value);
+
+  useEffect(() => {
+    onChange(input);
+  }, [input, onChange]);
+
+  useEffect(() => {
+    setInput(value);
+  }, [value]);
+
+  const [selected, setSelected] = useState(false);
+  return (
+    <div
+      className={`flex relative bg-uiPrimary-200 dark:bg-slate-900 rounded-lg flex-col justify-start items-center text-uiPrimary-400 dark:text-gray-300 fill-uiPrimary-400  ${className}`}
+    >
+      <input
+        className="bg-transparent h-12 w-full px-4 font-semibold appearance-none focus:outline-none dark:placeholder:text-gray-500"
+        type={"text"}
+        value={input}
+        onChange={(input) => setInput(input.target.value)}
+        placeholder={selected ? "" : hint}
+        onSelect={() => setSelected(true)}
+        onBlur={() => setSelected(false)}
+        spellCheck={false}
+      ></input>
+    </div>
+  );
+}
