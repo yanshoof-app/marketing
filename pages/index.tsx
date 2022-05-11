d7ac61e40e01c39b5ad79a6d4467f5cda4ebd9e8
Itay Oshri <itay.oshri@gmail.com>
2022-05-11 11:56:19 +0300

    FEATURE: Features section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,20 @@
 import type { NextPage } from 'next'
 import Layout from '../components/Layout'
-import FirstHome from '../components/home/first'
+import FirstHome from '../components/home/First'
 import Navbar from '../components/ui/Navbar'
 import Updates from '../components/updates'
+import TeacherSchedule from '../components/home/TeacherSchedule'
+import Features from '../components/home/Features'
 
 const Home: NextPage = () => {
   return (
-    <Layout className="flex flex-col justify-center items-center">
-      <Navbar />
+    <Layout className="flex flex-col justify-center items-center bg-primary-700">
+      <Navbar className=" text-white !bg-primary-700" />
       <div className="w-full flex h-24 justify-center items-center">
         <Updates />
       </div>
       <FirstHome />
+      <Features />
     </Layout>
   )
 }
