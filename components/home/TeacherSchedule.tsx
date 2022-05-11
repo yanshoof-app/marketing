55c7dc0abac84fd11485e61661f71c063223b39f
Itay Oshri <itay.oshri@gmail.com>
2022-05-11 11:56:47 +0300

    REFACTOR: Main section

---
+++ components/home/TeacherSchedule.tsx
@@ -0,0 +1,11 @@
+const TEACHER_SCHEDULE = 'מערכת מורה'
+
+export default function TeacherSchedule() {
+  return (
+    <div>
+      <div>
+        <h1>{TEACHER_SCHEDULE}</h1>
+      </div>
+    </div>
+  )
+}
--- components/home/first.tsx
+++ components/home/first.tsx
@@ -13,14 +13,14 @@ const ENTER_SITE = 'כניסה לאתר'
 
 export default function FirstHome() {
   return (
-    <div className="flex max-w-5xl cursor-default gap-9">
+    <div className="flex  w-full h-[50rem] cursor-default gap-9 justify-center mt-14">
       <div className="flex flex-col gap-4 w-fit mt-24">
         <div className="flex flex-col gap-2">
-          <div className="w-fit text-6xl font-bold">
+          <div className="w-fit text-6xl font-bold text-white">
             <p>{YOUR_SCHEDULE}</p>
             <p>{WITH_YOUR_LESSONS}</p>
           </div>
-          <div className="text-base text-gray-400 font-semibold">
+          <div className="text-base text-slate-300 font-semibold">
             <p>{DESCRIPTION[0]}</p>
             <p>{DESCRIPTION[1]}</p>
           </div>
