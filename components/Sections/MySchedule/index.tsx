15e32b1d7fd97e48616072b779df0d7e73aec205
Itay Oshri <itay.oshri@gmail.com>
2022-05-12 01:05:36 +0300

    REFACTOR: My Schedule section

--- components/Sections/MySchedule/index.tsx
+++ components/Sections/MySchedule/index.tsx
@@ -13,7 +13,7 @@ export default function MySchedule() {
   return (
     <div
       ref={mySchedule}
-      className="flex items-center justify-center w-full bg-gray-200 dark:bg-slate-850"
+      className="flex flex-col gap-16 items-center justify-center w-full bg-white dark:bg-gray-900"
     >
       <div className="w-full max-w-5xl flex flex-col gap-10 pt-16 items-start ">
         <div className="flex flex-col gap-7 text-primary-500">
@@ -21,13 +21,17 @@ export default function MySchedule() {
           <div className="flex flex-col items-left gap-4 dark:text-white text-black">
             <h1 className=" text-6xl font-extrabold">המערכת שלי</h1>
             <h2 className=" text-2xl font-semibold dark:text-gray-300">
-              המערכת שלי
+              תיאור
             </h2>
           </div>
         </div>
-        <div className="flex flex-col gap-4 max-w-3xl w-full h-[40rem] overflow-hidden p-2">
-          <DayPick day={date} onChange={changeDate} />
-          <Timetable lessons={timetable_example} day={date} />
+      </div>
+      <div className="flex justify-center w-full bg-gray-100 dark:bg-slate-850">
+        <div className="flex justify-start w-[64rem] -mt-8">
+          <div className="flex flex-col gap-4 w-full h-[40rem] overflow-hidden p-2">
+            <DayPick day={date} onChange={changeDate} />
+            <Timetable lessons={timetable_example} day={date} />
+          </div>
         </div>
       </div>
     </div>
