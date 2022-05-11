3fec4ea76a829fdc6cec45d7ba7d97ff2f4450d1
Itay Oshri <itay.oshri@gmail.com>
2022-05-11 11:55:58 +0300

    REFACTOR: Remove logs

--- components/updates/index.tsx
+++ components/updates/index.tsx
@@ -9,11 +9,10 @@ export interface IUpdatesProps {}
 
 export default function Updates() {
   const { version, label } = useUpdates()
-  console.log(label)
   return (
     version && (
       <Link href={`${VERSIONS_PATH}${version}`} passHref>
-        <a className="flex items-center h-12 bg-primary-500 hover:bg-primary-700 duration-75 rounded-xl px-4 text-white gap-2 cursor-pointer select-none">
+        <a className="flex items-center h-12 bg-primary-500 hover:bg-primary-900 duration-75 rounded-xl px-4 text-white gap-2 cursor-pointer select-none">
           <p className="font-bold text-sm">{NEW}</p>
           <div className="flex">
             <p>
