6918723653bc219f94878cec5a523d45725f33c8
Itay Oshri <itay.oshri@gmail.com>
2022-05-16 17:35:42 +0300

    FIX: env

--- hooks/useUpdates.ts
+++ hooks/useUpdates.ts
@@ -1,6 +1,6 @@
 export default function useUpdates() {
   return {
-    version: process.env.NEXT_PUBLIC_VERSION,
-    label: process.env.NEXT_PUBLIC_LABEL,
+    version: process.env.VERSION,
+    label: process.env.LABEL,
   }
 }
