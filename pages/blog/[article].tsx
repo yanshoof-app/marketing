cf0e1452635862990bcf3926205e8abc3f689bdd
Itay Oshri <itay.oshri@gmail.com>
2022-05-16 16:27:10 +0300

    FIX: Confilicted paths

--- pages/blog/[article].tsx
+++ pages/blog/[article].tsx
@@ -2,10 +2,8 @@ import PostBody from '../../components/blog/Body'
 import PostHead from '../../components/blog/Head'
 import Layout from '../../components/Layout'
 import Navbar from '../../components/ui/Navbar'
-import { BLOG_POST_EXAMPLE } from '../../samples'
 import { readFileSync } from 'fs'
 import path from 'path'
-import { useRouter } from 'next/router'
 
 const BlogArticale = (props) => {
   return (
@@ -24,20 +22,20 @@ const BlogArticale = (props) => {
 export async function getStaticProps({ params }) {
   const article = params.article
 
-  const full_path = `public/blog/posts/${article}`
+  const full_path = `public/blogPosts/posts/${article}`
 
   const POST = 'post.md'
   const post_path = path.join(full_path, POST)
   const post = readFileSync(post_path, 'utf-8')
 
-  const postInfo = require(`/public/blog/posts/${article}/info.json`)
+  const postInfo = require(`/public/blogPosts/posts/${article}/info.json`)
 
   return { props: { post: post, postInfo: postInfo } }
 }
 
 export async function getStaticPaths() {
   return {
-    paths: [{ params: { article: '' } }],
+    paths: [{ params: { article: 'timetable-1.0' } }],
     fallback: true,
   }
 }
