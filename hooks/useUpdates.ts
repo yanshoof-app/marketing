4eeffac21b9bb15273c4a92b47c84d306c4996ce
Itay Oshri <itay.oshri@gmail.com>
2022-05-30 19:05:40 +0300

    FIX: Problematic import

--- hooks/useUpdates.ts
+++ hooks/useUpdates.ts
@@ -1,6 +1,7 @@
-import { version, label } from '../public/updates.json'
+import update from '../public/updates.json'
 
 export default function useUpdates() {
+  const { version, label } = update
   return {
     version,
     label,
